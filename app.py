a99df3b9b585140ad8ebe4107b104a39cf9b6602
jeremy <jperret@clauger.fr>
2022-09-26 16:43:58 +0200

    update modvie details page

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
 from tmdb import tmdb, movies, tv, trendings
-from database.mydatabase import create_table, get_data, insert_to_db
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "your secret key"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 tmdb = tmdb.Tmdb()
-tmdb.api_key = "8455657f72d491d8c72563e88212ce94"
-tmdb.language = "fr-FR"
-tmdb.date_format = "%d/%m/%Y"
+tmdb.api_key = os.getenv("API_KEY")
+tmdb.language = os.getenv("LANGUAGE")
+tmdb.date_format = os.getenv("DATE_FORMAT")
+
 movies = movies.Movies()
 tv = tv.TV()
 trendings = trendings.Trendings()
-create_table()
 
 
 @app.route("/")
@@ -30,8 +33,10 @@ def index():
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id: int):
-    movie = get_data("movies_items", movie_id)
-    if movie == "[]":
-        movie = movies.details(movie_id)
-        # insert_to_db("movies_items", movie)
-    return render_template("movie-dev.html", entity=movie, date_format=tmdb.date_format)
+    movie = movies.details(movie_id)
+    return render_template(
+        "movie-dev.html",
+        entity=movie,
+        casts=movie["casts"]["cast"],
+        date_format=tmdb.date_format,
+    )
